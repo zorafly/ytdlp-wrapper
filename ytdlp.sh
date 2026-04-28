5091eec447230d3e9cda2a0cb2daad40ca24ddd9
April May <zorafly@icloud.com>
2026-04-28 09:20:21 -0400

    upgrade pip in update phase

--- ytdlp.sh
+++ ytdlp.sh
@@ -68,6 +68,7 @@ update() {
     echo -e "\n➜ Updating virtual environment...\n"
     set -ex
     source venv/bin/activate
+    pip install --upgrade pip
     pip install -U yt-dlp-ejs certifi build
     set +ex
 
